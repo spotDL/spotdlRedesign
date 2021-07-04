5a418f8811b10208e1ba8f6143b77e5a0d63a451
FreeCap23 <dionisiestratulat@gmail.com>
2021-07-04 20:30:29 +0300

    - Optimized code for pylint. - Used black reformatter

--- spotdl/check_down_ffmpeg.py
+++ spotdl/check_down_ffmpeg.py
@@ -5,10 +5,8 @@
 import platform
 import os
 import shutil
-import re
-import zipfile
-import subprocess
 import requests
+
 
 def download_ffmpeg():
     """
@@ -27,30 +25,40 @@ def download_ffmpeg():
         # !The 32bit values & Darwin's are educated guesses, as I do not have any 32bit machines
         # !Or any macOS devices.
         "Windows": {
-            "AMD64":    "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/win32-x64",
-            "i686":     "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/win32-ia32",
-            "arm":      "PUT SOMETHING HERE!!!!"
+            "AMD64": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/win32-x64",
+            "i686": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/win32-ia32",
+            "arm": "PUT SOMETHING HERE!!!!",
         },
         "Linux": {
-            "x86_64":   "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/linux-x64",
-            "x86":      "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/linux-ia32",
-            "arm32":    "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/linux-arm",
-            "aarch64":  "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/linux-arm64"
+            "x86_64": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/linux-x64",
+            "x86": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/linux-ia32",
+            "arm32": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/linux-arm",
+            "aarch64": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/linux-arm64",
         },
         "Darwin": {
-            "x86_64":   "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/darwin-x64",
-            "x86":      "https://lame.buanzo.org/ffmpeg-mac-2.2.2.zip",
-            "arm":      "https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/darwin-arm64"
-        }
+            "x86_64": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/darwin-x64",
+            "x86": "https://lame.buanzo.org/ffmpeg-mac-2.2.2.zip",
+            "arm": "https://github.com/eugeneware/"
+            "ffmpeg-static/releases/download/b4.4/darwin-arm64",
+        },
     }
 
     try:
         link_to_use = sources[platform.system()][platform.machine()]
-    except:
+    except Exception as exc:
         # Should change this message once we know for sure the output of the previous command
         # On any and all OS and Architectures listed.
-        raise Exception("Could not detect architecture or operating system. "
-                        "Please open an issue on the GitHub page of this project.")
+        raise Exception(
+            "Could not detect architecture or operating system. "
+            "Please open an issue on the GitHub page of this project."
+        ) from exc
     current_dir = os.path.dirname(__file__)
     ffmpeg_data = requests.get(link_to_use, allow_redirects=True).content
     if link_to_use == sources["Darwin"]["x86"]:
@@ -60,6 +68,7 @@ def download_ffmpeg():
         open(f"{current_dir}\\ffmpeg\\bin\\ffmpeg.exe", "wb").write(ffmpeg_data)
     else:
         open(f"{current_dir}/ffmpeg/bin/ffmpeg", "wb").write(ffmpeg_data)
+
 
 if shutil.which("ffmpeg") is not None:
     print("Found ffmpeg in path. No need to download!")
