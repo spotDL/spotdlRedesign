a1da147c2f65adda848b5eae585e9e45e61ec571
FreeCap23 <dionisiestratulat@gmail.com>
2021-07-04 12:06:59 +0300

    After black reformatting

--- spotdl/check_down_ffmpeg.py
+++ spotdl/check_down_ffmpeg.py
@@ -12,6 +12,7 @@ import zipfile
 import subprocess
 import requests
 
+
 def download_ffmpeg():
     """
     ### Args
@@ -24,16 +25,16 @@ def download_ffmpeg():
     - None
     """
     regex = r"ffmpeg.*.zip"
-    url = 'https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-06-30-12-38/ffmpeg-n4.4-78-g031c0cb0b4-win64-gpl-4.4.zip'
-    #Finds the name of the zip and cuts off the .zip part
+    url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-06-30-12-38/ffmpeg-n4.4-78-g031c0cb0b4-win64-gpl-4.4.zip"
+    # Finds the name of the zip and cuts off the .zip part
     name_of_zip = re.search(regex, url).group()[:-4]
-    open('ffmpeg.zip', 'wb').write(requests.get(url, allow_redirects=True).content)
+    open("ffmpeg.zip", "wb").write(requests.get(url, allow_redirects=True).content)
     with zipfile.ZipFile("ffmpeg.zip", "r") as ffmpeg_zip:
         ffmpeg_zip.extract(f"{name_of_zip}/bin/ffmpeg.exe")
 
-    #Clean up after extracting the zip file
+    # Clean up after extracting the zip file
     os.replace(f"{name_of_zip}/bin/ffmpeg.exe", "./ffmpeg.exe")
-    os.remove('ffmpeg.zip')
+    os.remove("ffmpeg.zip")
     try:
         os.remove(f"{name_of_zip}/bin/ffmpeg.exe")
     except:
@@ -44,9 +45,9 @@ def download_ffmpeg():
 
 try:
     devnull = open(os.devnull)
-    subprocess.Popen(['ffmpeg'], stdout=devnull, stderr=devnull).communicate()
+    subprocess.Popen(["ffmpeg"], stdout=devnull, stderr=devnull).communicate()
     print("Found ffmpeg in PATH!")
 except OSError as e:
-    if(not os.path.isfile("./ffmpeg.exe")):
+    if not os.path.isfile("./ffmpeg.exe"):
         print("ffmpeg not found, downloading it...")
         download_ffmpeg()
